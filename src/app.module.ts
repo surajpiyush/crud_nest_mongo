7d3049db9671f316f36ed3e1011308bc2d424210
piyush kumar <piyushkumar9648@gmail.com>
2023-01-17 14:20:05 +0530

    first

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.local.env'],
+    }),
+    
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    BookModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/book/book.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { BookService } from './book.service';
+import { BookController } from './book.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './schema/book';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+  ],
+
+  controllers: [BookController],
+  providers: [BookService],
+})
+export class BookModule {}
